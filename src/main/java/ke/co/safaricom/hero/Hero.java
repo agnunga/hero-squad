24ee041f3acf994f56ba9f3d57eb6181787d1cb6
Omondi Agunga <agunga3d@gmail.com>
2023-08-14 23:01:17 +0300

    Hero and Squad class creation

---
+++ src/main/java/ke/co/safaricom/hero/Hero.java
@@ -0,0 +1,42 @@
+package ke.co.safaricom.hero;
+
+public class Hero {
+
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public void setSpecialPower(String specialPower) {
+        this.specialPower = specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+}
---
+++ src/main/java/ke/co/safaricom/hero/Squad.java
@@ -0,0 +1,31 @@
+package ke.co.safaricom.hero;
+
+public class Squad {
+    private String maxSize;
+    private String name;
+    private String  dedicatedCause;
+
+    public String getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(String maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDedicatedCause() {
+        return dedicatedCause;
+    }
+
+    public void setDedicatedCause(String dedicatedCause) {
+        this.dedicatedCause = dedicatedCause;
+    }
+}
