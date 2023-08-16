0b7ddee95acb08d0f8820e976122a840c06dd238
Omondi Agunga <agunga3d@gmail.com>
2023-08-16 10:32:52 +0300

    added sparkjava

--- src/main/java/ke/co/saf/hero/Main.java
+++ src/main/java/ke/co/saf/hero/Main.java
@@ -1,7 +1,17 @@
 package ke.co.saf.hero;
 
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Here we are...");
+
+
+        staticFileLocation("/public");
+
+        get("/", (request, response) ->{
+            return "Here / ";
+        } );
+
     }
 }
