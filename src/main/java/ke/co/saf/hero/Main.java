10a5a6bb213055323ce40d8bebbdf06f6fc28a3a
Omondi Agunga <agunga3d@gmail.com>
2023-08-16 16:42:47 +0300

    updated build.gradle to add dependancies to .jar

--- src/main/java/ke/co/saf/hero/Main.java
+++ src/main/java/ke/co/saf/hero/Main.java
@@ -4,13 +4,13 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Here we are...");
+        System.out.println("Here we are updated...");
 
 
         staticFileLocation("/public");
 
         get("/", (request, response) ->{
-            return "Here / ";
+            return "Here updated / ";
         } );
 
     }
